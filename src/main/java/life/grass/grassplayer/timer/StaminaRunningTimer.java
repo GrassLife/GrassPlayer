12904a5cee89614e760f45ec90b6c9540439cade
unicroak <unicroak@gmail.com>
2017-06-29 23:39:29 +0900

    [improve] ボートのスタミナ消費を上方修正

--- src/main/java/life/grass/grassplayer/timer/StaminaRunningTimer.java
+++ src/main/java/life/grass/grassplayer/timer/StaminaRunningTimer.java
@@ -11,12 +11,11 @@ public class StaminaRunningTimer implements Runnable {
         Bukkit.getOnlinePlayers().forEach(player -> {
             GrassPlayer grassPlayer = GrassPlayer.findOrCreate(player);
 
-
             if (player.isSprinting()) {
                 grassPlayer.incrementStamina(5);
                 grassPlayer.incrementEffectiveStamina(Math.random() < 0.3D ? -1 : 0);
             } else if (player.getVehicle() instanceof Boat) {
-                grassPlayer.incrementEffectiveStamina(Math.random() < 0.5D ? -1 : 0);
+                grassPlayer.incrementEffectiveStamina(Math.random() < 0.15D ? -1 : 0);
             } else {
                 grassPlayer.incrementStamina(5);
             }
